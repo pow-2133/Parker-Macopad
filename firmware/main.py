0ef7ff5ce8cbdf637afe7dacbebae947fd5605e6
pow-2133 <parkeravery1307@gmail.com>
2025-12-23 22:49:00 -0400

    main.py

--- firmware/main.py
+++ firmware/main.py
@@ -1,1 +1,22 @@
+from kb import Keyboard
+from kmk.keys import KC
+
+keyboard = Keyboard()
+
+# Key layout:
+# SW1 -> Undo
+# SW2 -> Redo
+# SW3 -> Volume Up
+# SW4 -> Volume Down
+keyboard.keymap = [
+    [
+        KC.LCTL(KC.Z),   # Undo
+        KC.LCTL(KC.Y),   # Redo
+        KC.VOLU,         # Volume Up
+        KC.VOLD          # Volume Down
+    ]
+]
+
+if __name__ == "__main__":
+    keyboard.go()
 
